3ccadfd2e47a0c9c2bfe8253d6a81eb418354e7e
pkreddy45 <94917401+pkreddy45@users.noreply.github.com>
2024-10-02 16:52:02 +0530

    Flask.py

--- Flask.py
+++ Flask.py
@@ -4,19 +4,21 @@ import requests
 app = Flask(__name__)
 
 API_KEY = "a9b45491b7fe84334ac483d8" 
-API_URL = "https://api.exchangerate-api.com/v4/latest/USD"  
+API_URL = "https://api.exchangerate-api.com/v4/latest/"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     converted_amount = None
+    source_currency = None
     target_currency = None
 
     if request.method == 'POST':
         amount = request.form['amount']
+        source_currency = request.form['source_currency'].upper()
         target_currency = request.form['target_currency'].upper()
 
-        # Make an API call with the API key included in the request
-        response = requests.get(f"{API_URL}?apikey={API_KEY}")
+        # Make an API call with the selected source currency
+        response = requests.get(f"{API_URL}{source_currency}?apikey={API_KEY}")
         data = response.json()
 
         # Check if the target currency exists in the API data
@@ -26,10 +28,7 @@ def index():
         else:
             converted_amount = "Currency not supported."
 
-    if request.method == 'Get':
-        converted_amount = None     
-
-    return render_template('index.html', converted_amount=converted_amount, target_currency=target_currency)
+    return render_template('index.html', converted_amount=converted_amount, target_currency=target_currency, source_currency=source_currency)
 
 if __name__ == '__main__':
     app.run(debug=True)
